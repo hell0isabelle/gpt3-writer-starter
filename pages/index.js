c8da512454fc0a8857dca6317a9829a62320c5e4
Isabelle Ki <alonzo04@bu.edu>
2023-01-15 12:21:23 -0500

    adopting code to use case

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,64 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>leaf of faith. </title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>leaf of faith</h1>
+          </div>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>a new cannabis dictionary</h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here"
+          className="prompt-box"
+          value={userInput}
+          onChange={onUserChangedText} />;
+          <div className="prompt-buttons">
+          <a className="generate-button" onClick={callGenerateEndpoint}>
+          <div className="generate">
+          <p>Generate</p>
+      </div>
+    </a>
+        </div>
+        <div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+    </div>
+  </a>
+</div>-
+        {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
       </div>
       <div className="badge-container grow">
         <a
@@ -26,7 +68,7 @@ const Home = () => {
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>buildspace x isabelle, with love from boston</p>
           </div>
         </a>
       </div>
